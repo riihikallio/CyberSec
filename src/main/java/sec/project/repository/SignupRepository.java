f0121dc048db215c01f82f65186f61766acca3ad
Petri Riihikallio <petri.riihikallio@metis.fi>
2019-12-22 21:29:01 +0200

    Kunnossa

--- src/main/java/sec/project/repository/SignupRepository.java
+++ src/main/java/sec/project/repository/SignupRepository.java
@@ -7,6 +7,6 @@ import sec.project.domain.Signup;
 
 public interface SignupRepository extends JpaRepository<Signup, Long> {
 
-//    @Query("SELECT name, logo FROM SignUp WHERE name = ?1 AND logo IS NOT NULL")
-//    public List<Signup> findLogos(String name);
+    Signup findByName(String name);
+
 }
